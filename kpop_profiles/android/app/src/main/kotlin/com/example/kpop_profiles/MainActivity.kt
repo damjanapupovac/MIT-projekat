d061b6d3bea3d1c65432dbd4d91d4bfa8dd26ed9
damjanapupovac <asvejedno@gmail.com>
2025-12-19 14:33:26 +0100

    prvi commit

---
+++ kpop_profiles/android/app/src/main/kotlin/com/example/kpop_profiles/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kpop_profiles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ kpop_profiles/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'K-Pop Profiles',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('K-Pop Profiles'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to K-Pop Profiles',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
